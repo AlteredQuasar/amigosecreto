f7c2ea452fee7c6ec63574440d89860328f1502a
AlteredQuasar <86826119+AlteredQuasar@users.noreply.github.com>
2025-09-08 00:46:22 -0700

    Update app.js
    Agregue la funcion que permite ingresar los nombres presionando la tecla enter y ordene mejor las notas para que se pueda entender mejor el codigo

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+     //variable lista de amigos
     let amigos = [];
-
+    //permite ingresar los nombres del input con la tecla enter
+    document.getElementById('cajaTexto').addEventListener('keydown', function(event){
+  if(event.key === 'Enter'){
+    agregarAmigo();
+  }
+});
     //asignar texto a un elemento
     function asignarTextoElemento(elemento, texto){
         let elementoHTML = document.getElementById(elemento);
@@ -8,62 +14,55 @@
 }
     //agregar un amigo a nuestra lista
    function agregarAmigo(){
+    //value trim para que no hayan espacios entre los nombres
         let nombreAmigo = document.getElementById('cajaTexto').value.trim(); 
-         //value trim para que no hayan espacios entre los nombres
+        //si la caja esta vacia, mandar una alerta
         if (nombreAmigo === '') {
-            //si la caja esta vacia, mandar una alerta
             asignarTextoElemento('resultado', 'Ingresa un nombre valido.') ;
             resultado.style.color = 'red'; 
     }
+    //limpiar la caja en caso de haber ingresado un nombre repetido
     else if (amigos.includes(nombreAmigo)) {
         asignarTextoElemento('resultado', 'Ese nombre ya fue ingresado.') ;
         document.getElementById('cajaTexto').value = '';
-        //limpiar la caja en caso de haber ingresado un nombre repetido
-        resultado.style.color = 'orange'; 
         //si repiten un nombre, aparece un texto y cambia de color a naranja
+        resultado.style.color = 'orange'; 
     }
-    else { 
+    else { //mandar nombre de amigos a la lista 'amigos'
         amigos.push(nombreAmigo);
-        //mandar nombre de amigos a la lista 'amigos'
+        //mostrar nuestra nueva lista actualizada
         mostrarAmigos();
-        
-         //mostrar nuestra nueva lista actualizada
+         //limpiar la caja cuando se haya completado el push
         document.getElementById('cajaTexto').value = '';
-        //limpiar la caja cuando se haya completado el push
+       //limpiar texto de alerta en caso de haber ingresado un nombre invalido.
         limpiarTextoResultado();
-        //limpiar texto de alerta en caso de haber ingresado un nombre invalido.
     }
- } 
+ } //funcion para mostrar los amigos que se van a sortear
     function mostrarAmigos (){
-    //funcion para mostrar los amigos que se van a sortear
     let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = '';
     //para que se limpie la lista
+    lista.innerHTML = '';
+    //por cada amigo en la lista, se crea una lista en el HTML con cada nombre que se ingreso en el input.
     amigos.forEach((amigo) => {
         let li = document.createElement('li');
         li.textContent = "👤" + amigo
         lista.appendChild(li);
-        //por cada amigo en la lista, se crea una lista en el HTML con cada nombre que se ingreso en el input.
     })
-
-}
+} //sortea los amigos creando un numero aleatorio dependiendo el numero de nombres de la lista y luego pone el resultado.
     function sortearAmigo (){
         let numeroAleatorio = Math.floor(Math.random() * amigos.length);
-         let amigoSorteado = amigos[numeroAleatorio]; 
-        //sortea los amigos creando un numero aleatorio dependiendo el numero de nombres de la lista y luego pone el resultado.
-        if (amigos.length === 0) {
-            asignarTextoElemento('resultado', 'No hay ningun nombre en tu lista.') 
-            resultado.style.color = 'red'; 
-            //cambia a color rojo el mensaje
+        let amigoSorteado = amigos[numeroAleatorio]; 
+            if (amigos.length === 0) {
+                asignarTextoElemento('resultado', 'No hay ningun nombre en tu lista.') 
+                //cambia a color rojo el mensaje
+                resultado.style.color = 'red'; 
         }
-        else {
-             document.getElementById('resultado').textContent = "El amigo sorteado es: " + amigoSorteado;
-             resultado.style.color = 'limegreen'; 
+            else {
+                document.getElementById('resultado').textContent = "El amigo sorteado es: " + amigoSorteado;
+                resultado.style.color = 'limegreen'; 
         }
 }
-
     //funcion para limpiar el mensaje de alerta en caso de ingresar algun nombre invalido.
     function limpiarTextoResultado () {
-
         document.getElementById('resultado').textContent = "";
     }
